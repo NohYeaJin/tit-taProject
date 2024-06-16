9503ca274991d02f8fb8f526aa2f1c29e0443a86
NohYeaJin <yeajinny@naver.com>
2024-06-16 19:44:55 +0900

    알람설정 기능 추가

--- musical/admin.py
+++ musical/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Musicals, Categories, Locations, Genres, MainImages, Notice
+from .models import Musicals, Categories, Locations, Genres, MainImages, Notice, PushNotification, MusicalSeries, \
+    MusicalSource, TicketNotification, Users
 
 
 class CrawledMusicalAdmin(admin.ModelAdmin):
@@ -10,11 +11,29 @@ class CrawledMusicalAdmin(admin.ModelAdmin):
 class MainImageAdmin(admin.ModelAdmin):
     readonly_fields = ['img_preview']
 
+class TicketNotificationAdmin(admin.ModelAdmin):
+    readonly_fields = ['musical_id', 'user_id', 'created_at']
+
+@admin.register(Notice)
+class QuillPostAdmin(admin.ModelAdmin):
+    readonly_fields = ['created_at', 'updated_at']
+
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('user_id', 'user_email', 'user_phone', 'user_address', 'user_nickname', 'user_last_login')
+    def has_add_permission(self, request):
+        return False
+
+    def has_delete_permission(self, request, obj=None):
+        return False
+
 admin.site.register(Musicals, CrawledMusicalAdmin)
 admin.site.register(Categories)
 admin.site.register(Locations)
 admin.site.register(Genres)
 admin.site.register(MainImages, MainImageAdmin)
-@admin.register(Notice)
-class QuillPostAdmin(admin.ModelAdmin):
-    readonly_fields = ['created_at', 'updated_at']
+admin.site.register(PushNotification)
+admin.site.register(MusicalSeries)
+admin.site.register(MusicalSource)
+admin.site.register(TicketNotification, TicketNotificationAdmin)
+admin.site.register(Users, UserAdmin)
+
--- musical/models.py
+++ musical/models.py
@@ -12,7 +12,6 @@ class Musicals(models.Model):
     created_at = models.DateTimeField(default=timezone.now)
     open_from = models.DateTimeField(default=timezone.now)
     open_to = models.DateTimeField(default=timezone.now)
-    ticket_time = models.DateTimeField()
     location = models.ForeignKey('Locations', on_delete=models.CASCADE)  # 외래키 추가
     theater = models.CharField(max_length=100, blank=True)
     genre = models.ForeignKey('Genres', on_delete=models.CASCADE)  # 외래키 추가
@@ -31,13 +30,54 @@ class Musicals(models.Model):
     def img_preview(self):
         return mark_safe('<img src = "{url}" width = "300"/>'.format(url=self.poster))
 
+class PushNotification(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    created_at = models.DateTimeField(default=timezone.now)
+    updated_at = models.DateTimeField(auto_now=True)
+    push_at = models.DateTimeField(blank=True, null=True)
+
+class MusicalSeries(models.Model):
+    series_name = models.CharField(max_length=50)
+    series_description = models.TextField()
+    ticket_time = models.DateTimeField()
+    musical_id = models.ForeignKey('Musicals', on_delete=models.CASCADE)
+    cast = models.CharField(max_length=200, blank=True)
+    reservation_source = models.CharField(max_length=100, blank=True)
+
+
+    def __str__(self):
+        return self.musical_id.title +" - " + self.series_name
+
+class MusicalSource(models.Model):
+    source_name = models.CharField(max_length=50)
+    source_link = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.source_name
+
+class MusicalReservationLink(models.Model):
+    reservation_link = models.CharField(max_length=100)
+    series_id = models.ForeignKey('MusicalSeries', on_delete=models.CASCADE)
+    source_id = models.ForeignKey('MusicalSource', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'{self.series_id.series_name} - {self.source_id.source_name}'
+
 class Categories(models.Model):
     name = models.CharField(max_length=50)
 
     def __str__(self):
         return self.name
 
+class TicketNotification(models.Model):
+    musical_id = models.ForeignKey('Musicals', on_delete=models.CASCADE)
+    user_id = models.ForeignKey('Users', on_delete=models.CASCADE)
+    is_notified = models.BooleanField(default=False)
+    created_at = models.DateTimeField(default=timezone.now)
 
+    def __str__(self):
+        return self.musical_id.title
 
 class Locations(models.Model):
     location_name = models.CharField(max_length=50)
@@ -68,3 +108,15 @@ class Notice(models.Model):
 
     def __str__(self):
         return self.notice_title
+
+class Users(models.Model):
+    user_id = models.CharField(max_length=50)
+    user_email = models.EmailField()
+    user_password = models.CharField(max_length=50)
+    user_phone = models.CharField(max_length=50)
+    user_address = models.CharField(max_length=100)
+    user_nickname = models.CharField(max_length=50)
+    user_last_login = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return self.user_id + " - " + self.user_last_login.strftime('%Y-%m-%d %H:%M:%S')
